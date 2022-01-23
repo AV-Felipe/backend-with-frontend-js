c8dd197906041f8cee301afa4945f5cb3f25092d
AV-Felipe <aerogrado@hotmail.com>
2022-01-23 18:49:13 -0300

    Improve query form system

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
 // IMPORTS
-import { createTableHeader, populateData } from "./modules/data-to-html.js";
+import { createTableHeader, populateData, updateValueList } from "./modules/data-to-html.js";
 
 // ELEMENTS
 
@@ -7,13 +7,16 @@ const inputQueryType = document.getElementById('query-type');
 const inputQueryValue = document.getElementById('main-search-input');
 const buttonSendQuery = document.getElementById('send-query-button');
 const outputTable = document.getElementById('fetched-data-table');
-const outputTableBody = document.querySelector('#fetched-data-table tbody');
+const formFieldForQueryValue = document.getElementById('main-search-input-area');
+
 
 // LISTENERS
 
 buttonSendQuery.addEventListener('click', sendQuery);
 
-// FUNCTIONS
+inputQueryType.addEventListener('change', updateQueryValues);
+
+// FETCH FUNCTIONS
 
 // GET request - route /people - !!! it is actually performing three functions, preparing the display table and fetching data before rendering it
 //we could slit in one function that prepares the table and calls the fetch, then the fetch, on finishing, pass the data to a rendering function
@@ -32,3 +35,18 @@ function sendQuery(){
         });
 }
 
+// FUNCTIONS
+
+function updateQueryValues() {
+
+    while(inputQueryValue.firstChild){
+        inputQueryValue.removeChild(inputQueryValue.firstChild);
+    }
+    if (inputQueryType.value === 'month' || inputQueryType.value === 'department'){
+        formFieldForQueryValue.style.display = 'block'
+        inputQueryValue.insertAdjacentHTML('beforeend', updateValueList(inputQueryType.value));
+    }
+    if (inputQueryType.value === 'branch'){
+        formFieldForQueryValue.style.display = 'none';
+    }
+}
--- frontend/src/modules/data-to-html.js
+++ frontend/src/modules/data-to-html.js
@@ -65,5 +65,44 @@ function populateData(dataObject, tableKind){
     
 }
 
+function updateValueList(queryType){
+    if(queryType === 'month'){
+        return (`
+            <option value="01">janeiro</option>
+            <option value="02">fevereiro</option>
+            <option value="03">março</option>
+            <option value="04">abril</option>
+            <option value="05">maio</option>
+            <option value="06">junho</option>
+            <option value="07">julho</option>
+            <option value="08">agosto</option>
+            <option value="09">setembro</option>
+            <option value="10">outubro</option>
+            <option value="11">novembro</option>
+            <option value="12">dezembro</option>
+        `);
+    }
 
-export {createTableHeader, populateData,};
+    if(queryType === 'department'){
+        return (`
+            <option value="Health Care">Health Care</option>
+            <option value="Consumer Durables">Consumer Durables</option>
+            <option value="Consumer Non-Durables">Consumer Non-Durables</option>
+            <option value="n/a">n/a</option>
+            <option value="Miscellaneous">Miscellaneous</option>
+            <option value="Capital Goods">Capital Goods</option>
+            <option value="Basic Industries">Basic Industries</option>
+            <option value="Consumer Services">Consumer Services</option>
+            <option value="Technology">Technology</option>
+            <option value="Public Utilities">Public Utilities</option>
+            <option value="Finance">Finance</option>
+            <option value="Energy">Energy</option>
+            <option value="Transportation">Transportation</option>
+        `);
+        
+
+    }
+}
+
+
+export {createTableHeader, populateData, updateValueList};
